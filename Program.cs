75b5d779c556d7d41371e42ab2e1cfc8960ccbfb
Brand0nLe <93brandonle@gmail.com>
2022-10-20 10:46:31 -0700

    Final Commit

--- Program.cs
+++ Program.cs
@@ -1,30 +1,59 @@
 ﻿//Brandon Le
-//10-18-22
+//10-20-22
 //Mini Challenge 3 - Asking Questions
+//Project will ask user 4 different questions and store the answers into 4 different
+//strings and it will print it back out to them in various sentences in the terminal.
+//Errors will pop up if they enter in numbers.
 
-//need to add data validation
 bool playAgain = true;
 string userInput = "";
 string userInput2 = "";
 string userInput3 = "";
 string userInput4 = "";
+bool canParse;
+int validNum = 0;
 
 
 while (playAgain == true)
 {
     Console.WriteLine("Hi there I haven't seen you before. What's your name?");
     userInput = Console.ReadLine();
+    canParse = Int32.TryParse(userInput, out validNum);
+    if(canParse == true)
+    {
+        Console.WriteLine("INVALID ENTRY. Please enter your name:");
+        userInput = Console.ReadLine();
+    }
+
     Console.WriteLine($"Oh nice to meet you {userInput}!");
     Console.WriteLine("Okay I gotta know, where are you from?");
     userInput2 = Console.ReadLine();
+    if(canParse == true)
+    {
+        Console.WriteLine("INVALID ENTRY. Please tell me where you're from:");
+        userInput2 = Console.ReadLine();
+    }
+
     Console.WriteLine($"Wow you're from {userInput2}?! That's so cool!");
     Console.WriteLine("Okay okay okay last question...");
     Console.WriteLine("What is your number 1 favorite food in the world?");
     userInput3 = Console.ReadLine();
+    if(canParse == true)
+    {
+        Console.WriteLine("INVALID ENTRY. Please enter favorite food:");
+        userInput3 = Console.ReadLine();
+    }
+
+    Console.WriteLine("\n----------------------------------------------------------");
     Console.WriteLine($"Wow how funny {userInput3} is also one of my favorites too!");
     Console.WriteLine($"Okay {userInput} so far I've learned that you're from {userInput2}, you love to eat {userInput3}, hmmm what else should I know about you so I can guess your passwo- I mean so I can get to know you better :)");
     Console.WriteLine($"Do you want to play again {userInput}? (YES/NO)");
     userInput4 = Console.ReadLine().ToUpper();
+    if(canParse == true)
+    {
+        Console.WriteLine("INVALID ENTRY. Please enter YES or NO:");
+        userInput4 = Console.ReadLine().ToUpper();
+    }
 
     if(userInput4 == "YES")
     {
@@ -35,5 +64,6 @@ while (playAgain == true)
         playAgain = false;
         Console.WriteLine($"Hehe okay well it was nice getting to know you {userInput} from {userInput2}!");
     }
+
 
 }
